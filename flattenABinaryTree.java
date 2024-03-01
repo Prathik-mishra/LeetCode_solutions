2b8b71edcab78dd069356d5d69ef53e54b6e5fa9
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-03-01 19:03:51 +0530

    Add files via upload

---
+++ flattenABinaryTree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        //first recursive approach: T.C :- O(N) and S.C :- O(N)
+         if(root == null){
+            return;
+        }
+        
+        flatten(root.right);
+        flatten(root.left);
+        
+        root.right = prev;
+        root.left = null;
+        prev = root;
+        
+        //stack approach: T.C :- O(N) and S.C :- O(N)
+        /*
+        if(root == null){
+            return;
+        }
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode curr = st.peek();
+            st.pop();
+            
+            if(curr.right != null){
+                st.push(curr.right);
+            }
+            if(curr.left != null){
+                st.push(curr.left);
+            }
+            
+            if(!st.isEmpty()){
+                curr.right = st.peek();
+            }
+            curr.left = null;
+        }
+        */
+        
+        
+    }
+}
