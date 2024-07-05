07743efc5bd5464f45557f1f2e22f11af172f77e
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-07-05 20:51:07 +0530

    Create inOrderSuccessor.java

---
+++ BT and BST/inOrderSuccessor.java
@@ -0,0 +1,33 @@
+/User function Template for Java
+
+/*Complete the function below
+Node is as follows:
+class Node{
+	int data;
+	Node left,right;
+	Node(int d){
+		data=d;
+		left=right=null;
+	}
+}
+*/
+class Solution
+{
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+	public Node inorderSuccessor(Node root,Node x)
+         {
+          //add code here.
+          Node successor = null;
+          
+          while(root != null){
+              if(x.data >= root.data){
+                  root = root.right;
+              }else{
+                  successor = root;
+                  root = root.left;
+              }
+          }
+          
+          return successor;
+         }
+}
