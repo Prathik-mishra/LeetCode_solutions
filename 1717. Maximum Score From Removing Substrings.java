313710b75712c20cfe63111b0f474ce205a9ecf6
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-07-12 16:13:06 +0530

    Update 1717. Maximum Score From Removing Substrings.java

--- 1717. Maximum Score From Removing Substrings.java
+++ 1717. Maximum Score From Removing Substrings.java
@@ -24,6 +24,8 @@ class Solution {
         return sb.reverse().toString();
     }
     public int maximumGain(String s, int x, int y) {
+        /*
+        // approach 1 : using stack
         Stack<Character> st = new Stack<>();
         int priority = (x >= y) ? x : y;
         int a,b;
@@ -39,5 +41,37 @@ class Solution {
             b = getScoreOfRemovedSubString(s,new Stack<Character>(),"ab",x);
         }
         return a+b;
+        */
+        
+        //approach 2 : optimised greedy counting
+        if(x >= y){
+            return getMaxScore(s,x,y,"ab");
+        }else{
+            return getMaxScore(s,y,x,"ba");
+        }
+    }
+    
+    private int getMaxScore(String s, int x, int y, String pattern){
+        int firstCount = 0, secondCount = 0,totalScore = 0;
+        char first = pattern.charAt(0);
+        char second = pattern.charAt(1);
+        
+        for(char ch : s.toCharArray()){
+            if(ch == first){
+                firstCount++;
+            }else if(ch == second){
+                if(firstCount > 0){
+                    firstCount--;
+                    totalScore += x;
+                }else{
+                    secondCount++;
+                }
+            }else{
+                totalScore += (Math.min(firstCount,secondCount) * y);
+                firstCount = secondCount = 0;
+            }
+        }
+        totalScore += (Math.min(firstCount,secondCount) * y);
+        return totalScore;
     }
 }
